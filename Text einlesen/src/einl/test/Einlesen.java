67a61cb1bf7bb93f4d607e001b984ec3172672ef
cf051 <chris.funke.93@gmail.com>
2016-04-10 18:44:23 +0200

    einlesen leicht modifiziert (noch nicht lauffähig)

--- Text einlesen/src/einl/test/Einlesen.java
+++ Text einlesen/src/einl/test/Einlesen.java
@@ -11,35 +11,35 @@ public class Einlesen {
 	public static void main(String[] args) {
 		File myFile = new File(
 				"C:\\Users\\Christopher\\Desktop\\TestOrdner\\ShakespearePlaysPlus\\TXT\\historical\\The First Part of King Henry VI.txt");
+		
+		
 		if (myFile.exists()) {
 			try {
 				FileReader fR = new FileReader(myFile);
 				BufferedReader bR = new BufferedReader(fR);
 				String zeile = null;
+				StringBuilder builder = new StringBuilder();
 				while ((zeile = bR.readLine()) != null) {
-//					System.out.println(zeile);
-
-
+					builder.append(zeile).append("/n");
+				}
 					
+			String	ganzertext = builder.toString();
+			
+			
+
+/**					
 					String patternOpen = "<([^/>a-z]+)>";
-//					String patternClose = "</([^>]+)>";
 					
 					Pattern r = Pattern.compile(patternOpen);
-//					Pattern r2 = Pattern.compile(patternClose);
 					
 					Matcher m = r.matcher(zeile);
-//					Matcher m2 = r2.matcher(zeile);
 					
 					if(m.find()){
 						System.out.println("Found value " + m.group(1));
 						}
 					
-//					if(m2.find()){
-//						System.out.println("Found value"+ m2.group(0));
-//					}
-						
+	*/					
 					
-				}
 				bR.close();
 			} catch (Exception ex) {
 				
@@ -49,3 +49,9 @@ public class Einlesen {
 		}
 	}
 }
+//					String patternClose = "</([^>]+)>";
+//					Pattern r2 = Pattern.compile(patternClose);
+//					Matcher m2 = r2.matcher(zeile);
+//					if(m2.find()){
+//						System.out.println("Found value"+ m2.group(0));
+//					}
